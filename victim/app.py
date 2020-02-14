c6d6ca1d3381d78b6db530d3da0b30a80ba6fdc1
TasosLep <tasoslepipas@gmail.com>
2020-02-14 22:24:34 +0000

    app.py

--- victim/app.py
+++ victim/app.py
@@ -13,7 +13,7 @@ def loginVictim():
              return redirect(url_for('hello'))
         else: 
             varError = 'The credentials are wrong! Try again.'
-    return render.template('login.html', error = varError)
+    return render.template('/templates/login.html', error = varError)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
