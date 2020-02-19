7663d0f3a95a8210964fa7ecf07fc70302bce1cf
Dan Ristea <dan.ristea@zerodeposit.com>
2020-02-19 23:16:00 +0000

    show verbose dns response in sniffer

--- victim/sniff.py
+++ victim/sniff.py
@@ -1,3 +1,4 @@
+from scapy.layers.dns import DNS
 from scapy.layers.inet import IP
 from scapy.sendrecv import sniff
 
@@ -6,6 +7,7 @@ def handler(packet):
     print("length:", len(packet))
     print("source:", packet[IP].src)
     print("destination:", packet[IP].dst)
+    print("packet:", packet[DNS].show())
 
 
 sniff(filter="udp port 53", prn=handler)
