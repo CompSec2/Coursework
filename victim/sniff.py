79a5339c1028dae9975c2e3d05defcde7a517951
Dan Ristea <dan.ristea@zerodeposit.com>
2020-02-19 14:57:35 +0000

    print useful info from sniffer

--- victim/sniff.py
+++ victim/sniff.py
@@ -1,9 +1,11 @@
+from scapy.layers.inet import IP
 from scapy.sendrecv import sniff
 
 
 def handler(packet):
-    print(len(packet))
-    print([0])
+    print("length:", len(packet))
+    print("source:", packet[IP].src)
+    print("destination:", packet[IP].dst)
 
 
 sniff(filter="udp port 53", prn=handler)
