c677c0b3572cac1008771ffc69fd324b9a904d85
Tasos Lepipas <TasosLep@users.noreply.github.com>
2020-03-12 10:29:51 +0000

    Add files via upload

--- victim/app.py
+++ victim/app.py
@@ -1,5 +1,8 @@
 from flask import Flask, request, render_template, url_for, session, redirect
+import logging 
 app = Flask(__name__)
+
+logging.basicConfig(filename='demo.log', level=logging.DEBUG)
 
 @app.route('/loggedIn')
 def hello():
@@ -10,9 +13,11 @@ def loginVictim():
     varError = ''
     if request.method == 'POST':
         if request.form['password'] == 'toor' and request.form['username'] == 'root':
+	     app.logger.info('Processing default request')
              return redirect(url_for('hello')) 
-        varError = 'The credentials are wrong! Try again.'         
+        varError = 'The credentials are wrong! Try again.' 
+    app.logger.info('Processing default request')        
     return render_template('loginForm.html',  error = varError)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=4444, debug=True)
