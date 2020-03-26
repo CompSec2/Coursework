505610acd5fdbdd96b7367047c40abb640120414
Dan Ristea <dan.ristea@zerodeposit.com>
2020-03-26 13:36:25 +0000

    Attempt to fix docker wsgi

--- victim/app/app.py
+++ victim/app/app.py
@@ -1,24 +1,28 @@
 from flask import Flask, request, render_template, url_for, session, redirect
-import logging 
+import logging
+
 app = Flask(__name__)
 
 logging.basicConfig(filename='demo.log', level=logging.DEBUG)
 
+
 @app.route('/loggedIn')
 def hello():
-    app.logger.info('Processing default request')        
+    app.logger.info('Processing default request')
     return 'I hope nothing bad happens to me!'
 
-@app.route('/', methods=['GET','POST'])
+
+@app.route('/', methods=['GET', 'POST'])
 def loginVictim():
     varError = ''
     if request.method == 'POST':
         if request.form['password'] == 'toor' and request.form['username'] == 'root':
             app.logger.info('Processing default request')
-            return redirect(url_for('hello')) 
-        varError = 'The credentials are wrong! Try again.' 
-    app.logger.info('Processing default request')        
-    return render_template('loginForm.html',  error = varError)
+            return redirect(url_for('hello'))
+        varError = 'The credentials are wrong! Try again.'
+    app.logger.info('Processing default request')
+    return render_template('loginForm.html', error=varError)
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0')
